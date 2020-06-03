119efc3e6f766e9ff075d09ae67b92950eda1a83
Dalvir Singh <dalvir.singh@cerner.com>
2020-06-02 22:20:34 -0400

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,4 @@ Rails.application.routes.draw do
              controllers: {
                sessions: 'host/sessions', registrations: 'host/registrations'
              }
-
-  namespace :api do
-    namespace :v1, defaults: { format: :json } do
-      resources :users, only: %i[index create]
-      resources :events, only: %i[index create hosts_events]
-      get '/host/events' => 'events#host_events', as: :host_events
-    end
-  end
 end
