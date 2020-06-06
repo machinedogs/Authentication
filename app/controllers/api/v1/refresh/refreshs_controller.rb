2335f2e5238c3c3b09d36671df5e9d78a8445e0c
Dalvir Singh <dalvir.singh@cerner.com>
2020-06-06 17:50:55 -0400

    refresh

--- app/controllers/api/v1/refresh/refreshs_controller.rb
+++ app/controllers/api/v1/refresh/refreshs_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::Refresh::RefreshsController < ApplicationController
         
         if Time.at(@refresh_tokens[:exp]) > Time.now
             #If not expired, give back new JWT token and new refresh token
-            render :refresh_tokens, status: :ok
+            render :refresh_tokens,:formats =>:json, status: :ok
         elsif @refresh_tokens[:refresh]!=true
             render json: { error: 'Token is not valid'}, status: :unauthorized
         else 
