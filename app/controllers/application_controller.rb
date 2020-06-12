74060d543e21297898e727049acc1a0b3954207d
Dalvir Singh <dalvir.singh@cerner.com>
2020-06-12 13:20:12 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::API
-  before_action :authenticate_api
+  # before_action :authenticate_api
 
-  private
-  #Use this method to verify that all api calls have a api key
-  def authenticate_api
-    return true if params['API_KEY'] == APP_CONFIG['API_KEY']
-    render json: { error: 'API KEY NOT PROVIDED'}, status: :unauthorized
-  end
+  # private
+  # #Use this method to verify that all api calls have a api key
+  # def authenticate_api
+  #   return true if params['API_KEY'] == APP_CONFIG['API_KEY']
+  #   render json: { error: 'API KEY NOT PROVIDED'}, status: :unauthorized
+  # end
 end
