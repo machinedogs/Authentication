75a896cf4ed9f567288e5fc28087e19ee4bfa3ee
Dalvir Singh <dalvir.singh@cerner.com>
2020-06-12 11:50:03 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::API
   #Use this method to verify that all api calls have a api key
   def authenticate_api
     return true if params['API-KEY'] == '31dd93e788c040367cb7b9bba52ff9d489dee5f05f7548b9313dd2d12c7981035b67e02abad9194d16c25ac0a88634c742301c77a3e56e2a5826d8a6216621f5'
+    render json: { error: 'API KEY NOT PROVIDED'}, status: :unauthorized
   end
 end
