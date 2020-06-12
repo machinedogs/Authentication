a818f3fdcb76e11b6265f2b8f4619d38d9a13f50
Dalvir Singh <dalvir.singh@cerner.com>
2020-06-12 10:45:44 -0400

    Update host.rb

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Host < ApplicationRecord # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable # Include default devise modules. Others available are:
-  has_many :event
+  has_many :event, :dependent => :destroy
   devise :database_authenticatable,
          :registerable,
          :recoverable,
